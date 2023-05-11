a627125b64f0a2d9354a98307c659a3ebca46acc
zhaobowen <1964471757@qq.com>
2023-05-11 19:57:49 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import './App.css';
 import routerConfig from './router/index'
+import 'antd/dist/reset.css'
 
 function App() {
   return (
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,22 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom';
+import { Layout } from 'antd';
+import styles from './ManageLayout.module.less'
+
+const { Header, Content, Footer } = Layout;
+
 const MainLayout:FC = () => {
   return (
-    <>
-      <div>MainLayout</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>Logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>Footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>Footer</Footer>
+    </Layout>
   )
 }
 export default MainLayout;
