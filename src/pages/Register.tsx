860457c3b79ce4c88e7645781d2199423cf9a069
zhaobowen <1964471757@qq.com>
2023-05-19 11:54:28 +0800

    init register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,71 @@
 import React from 'react'
+import { Button, Form, Input, Space, Typography } from "antd";
+import { UserAddOutlined } from '@ant-design/icons';
+import styles from './Register.module.less'
+import { Link } from 'react-router-dom';
 
+
+const { Title } = Typography;
+
+// 注册页面
 export default function Register() {
+  const onFinish = (value : any) => {
+
+  }
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}><UserAddOutlined/></Title>
+          <Title level={2}>注册新用户</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          onFinish={onFinish}
+        > 
+          <Form.Item 
+            label="用户名"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="确认密码"
+            name="confrim"
+            rules={[{ required: true, message: '请确认密码' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="昵称"
+            name="nickname"
+            rules={[{ required: true, message: '请确认昵称' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType='submit'>
+                注册
+              </Button>
+              <Link to="/login ">已有账户，请登录</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
