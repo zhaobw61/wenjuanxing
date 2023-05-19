ba6051178a69b12a8a1c8b6fc9c070183e1a115e
zhaobowen <1964471757@qq.com>
2023-05-19 14:37:15 +0800

    add remeber login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,12 +2,45 @@ import { Button, Checkbox, Form, Input, Space, Typography } from "antd";
 import { UserAddOutlined } from '@ant-design/icons';
 import styles from './Register.module.less'
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const { Title } = Typography;
 
+const USERNAME_KEY = 'USERNAME'
+const PASSWORD_KEY = 'PASSWORD'
+
+function remenberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME_KEY, username);
+  localStorage.setItem(PASSWORD_KEY, password);
+}
+
+function deleteUserFromStorage() {
+  localStorage.removeItem(USERNAME_KEY);
+  localStorage.removeItem(PASSWORD_KEY);
+}
+
+function getUserInfoFromStorage() {
+  return {
+    username : localStorage.getItem(USERNAME_KEY),
+    password : localStorage.getItem(PASSWORD_KEY)
+  }
+}
+
+
 export default function Login() {
-  const onFinish = (value : any) => {
-    console.log(value)
+  const [ form ] = Form.useForm(); // 第三方的 hook
+  useEffect(() => {
+    const { username, password } = getUserInfoFromStorage()
+    form.setFieldsValue({ username, password })
+  }, [])
+  
+  const onFinish = (values : any) => {
+    const { username, password, remember } = values;
+    if(remember) {
+      remenberUser(username, password);
+    } else {
+      deleteUserFromStorage()
+    }
   }
   return (
     <div className={styles.container}>
