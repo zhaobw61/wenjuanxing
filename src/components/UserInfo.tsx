0e66b538ff14b2e70cc29c151f0ecf4ee5f54c44
zhaobowen <1964471757@qq.com>
2023-06-02 15:40:19 +0800

    添加用户信息模块

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useRequest } from 'ahooks';
+import { Link, useNavigate } from 'react-router-dom'
+import { UserOutlined } from '@ant-design/icons'; 
+import { getUserInfoService } from '../services/user';
+import { Button } from 'antd';
+import { removeToken } from '../utils/user-token';
 
 export default function UserInfo() {
+  const nav = useNavigate();
+  const { data } = useRequest(async() => {
+    const data = await getUserInfoService()
+    return data;
+  })
+  const { username, nickname } = data || {};
+  function logout() {
+    removeToken() // clear token
+    nav('/login')
+  }
+  const UserInfo = (
+    <>
+      <span style={{color: '#e8e8e8'}}>
+        <UserOutlined />
+        {nickname}
+      </span>
+      <Button type="link" onClick={logout}>退出</Button>
+    </>
+  )
+  const Login = (
+    <Link to="/login">登录</Link>
+  )
   return (
     <div>
-      <Link to="/login">登录</Link>
+      {username ?  UserInfo : Login}
     </div>
   )
 }
