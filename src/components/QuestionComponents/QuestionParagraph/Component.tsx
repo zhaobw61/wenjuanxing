4e5bbf61c2bfcd1b9a304d3d0ed906f027d60e68
zhaobowen <1964471757@qq.com>
2023-06-13 11:58:15 +0800

    添加换行

--- src/components/QuestionComponents/QuestionParagraph/Component.tsx
+++ src/components/QuestionComponents/QuestionParagraph/Component.tsx
@@ -7,10 +7,12 @@ const { Paragraph } = Typography
 
 const Component: FC<QuestionParagraphPropsType> = (props: QuestionParagraphPropsType) => {
   const { text = '', isCenter } = {...QuestionParagraphDefaultProps, ...props};
+
+  const t = text.replaceAll('\n', '<br>')
   
   return (
     <Paragraph style={{textAlign: isCenter ? 'center' : 'start', marginBottom: '0'}} >
-      { text }
+      <span dangerouslySetInnerHTML={{__html: t}}></span>
     </Paragraph>
   )
 }
