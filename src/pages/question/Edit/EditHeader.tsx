bd6f4fe9a73024ff516676501460b88fec323598
zhaobowen <1964471757@qq.com>
2023-06-28 15:20:23 +0800

    feat: 添加拖拽和排序

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,11 +1,14 @@
 import React, { FC, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { Button, Typography, Space, Input } from 'antd'
-import { EditOutlined, LeftOutlined } from '@ant-design/icons'
+import { Button, Typography, Space, Input, message } from 'antd'
+import { EditOutlined, LeftOutlined, LoadingOutlined } from '@ant-design/icons'
 import styles from './EditHeader.module.less'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useDebounceEffect, useKeyPress, useRequest } from 'ahooks'
 import EditToolbar from './EditToolbar'
 import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import { updateQuestionService } from '../../../services/question'
 import { changePageTitle } from '../../../store/pageInfoReducer'
 
 const { Title } = Typography;
@@ -37,6 +40,56 @@ const TitleElem: FC = () => {
   )
 }
 
+// 保存按钮
+const SaveButton: FC = () => {
+  const { id } = useParams() 
+  const { componentList } = useGetComponentInfo()
+  const pageInfo = useGetPageInfo()
+  const { loading, run: save } = useRequest(async () => {
+    if(!id) return
+    await updateQuestionService(id, {...pageInfo, componentList})
+  }, { manual: true })
+
+  // 自动保存
+  useDebounceEffect(() => {
+    save()
+  }, [componentList, pageInfo], {
+    wait: 1000
+  })
+
+  // 快捷键
+  useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+    event.preventDefault()
+    if(!loading) save()
+  })
+  return <Button onClick={save} disabled={loading} icon={loading ? <LoadingOutlined/> : null}>保存</Button>
+}
+
+// 发布按钮
+const PublishButton: FC = ()  => {
+  const nav = useNavigate();
+  const { id } = useParams() 
+  const { componentList } = useGetComponentInfo()
+  const pageInfo = useGetPageInfo()
+
+  const { loading, run: pub } = useRequest(async () => {
+    if(!id) return
+    await updateQuestionService(id, {
+      ...pageInfo,
+      componentList,
+      isPublished: true // 标志着问卷已经发布
+    })
+  }, { 
+    manual: true,
+    onSuccess: () => {
+      message.success('发布成功')
+      nav('/question/stat/'+ id) // 跳转到统计页
+    }
+  })
+
+  return <Button onClick={pub} disabled={loading} icon={loading ? <LoadingOutlined/> : null}>发布</Button>
+}
+
 const EditHeader: FC = () => {
   const nav = useNavigate();
   return <div className={styles['header-wrapper']}>
@@ -52,8 +105,8 @@ const EditHeader: FC = () => {
       </div>
       <div className={styles['right']}>
         <Space>
-          <Button>保存</Button>
-          <Button type='primary'>发布</Button>
+          <SaveButton />
+          <PublishButton />
         </Space>
       </div>
     </div>
